7f7a5a0dff02132e8384014047eb50902173d17c
Ryan Carr <rycarr@gmail.com>
2018-08-25 18:01:13 -0700

    Documented expected output

--- main.go
+++ main.go
@@ -2,6 +2,20 @@ package main
 
 import ("fmt")
 
+        /*******************
+         * Expected output
+         * ----------------
+         * 0
+         * 1
+         * 10
+         * 11
+         * 100
+         * 101
+         * 110
+         * 111
+         * 1000
+         * 1001
+         *******************/
 func main() {
     for i := 0; i < 10; i++ {
         // %b automatically converts the number to base 2
