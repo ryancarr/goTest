d4784aaa9df296e0c06f12bf80b4c3cdea3d10ef
Ryan Carr <rycarr@gmail.com>
2018-08-25 18:36:21 -0700

    Used go fmt to format code

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import ("fmt")
+import (
+	"fmt"
+)
 
 /*******************
 * Expected output
@@ -17,8 +19,8 @@ import ("fmt")
 * 1001
 *******************/
 func main() {
-    for i := 0; i < 10; i++ {
-        // %b automatically converts the number to base 2
-        fmt.Printf("%b\n", i);
-    }
+	for i := 0; i < 10; i++ {
+		// %b automatically converts the number to base 2
+		fmt.Printf("%b\n", i)
+	}
 }
